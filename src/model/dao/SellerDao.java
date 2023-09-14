b5c5697d18fde6c01c176a78401949d8949bbf0f
Pedro Augusto <pedroaugustocampos09@gmail.com>
2023-09-14 16:49:36 -0300

    delete implementation

--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -9,7 +9,7 @@ import java.util.List;
 public interface SellerDao {
     void insert (Seller obj);
     void update (Seller obj);
-    void deleteId(Integer id);
+    void deleteById(Integer id);
     Seller findById(Integer id);
 
     List<Seller> findAll();
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -82,8 +82,26 @@ public class SellerDaoJDBC implements SellerDao {
         }
     }
     @Override
-    public void deleteId(Integer id) {
+    public void deleteById(Integer id) {
+    PreparedStatement st = null;
+    try{
+        st = conn.prepareStatement("DELETE FROM seller WHERE Id = ?");
 
+        st.setInt(1, id);
+
+      int rows = st.executeUpdate();
+      if(rows == 0){
+          throw new DbException("Please check the ID provided and try again.");
+
+      }
+
+     }
+    catch (SQLException e){
+        throw new DbException(e.getMessage());
+    }
+    finally {
+       DB.closeStatement(st);
+    }
     }
 
     @Override
